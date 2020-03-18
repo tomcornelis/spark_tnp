058e7abe24a1487ecb42982957df491ea2762791
Devin Taylor <dntaylor@ucdavis.edu>
2020-03-18 19:56:49 +0100

    flake8

--- TagAndProbeFitter.py
+++ TagAndProbeFitter.py
@@ -1,6 +1,7 @@
 import ROOT
 ROOT.gROOT.SetBatch()
 
+
 class TagAndProbeFitter:
 
     def __init__(self, name):
@@ -13,17 +14,19 @@ class TagAndProbeFitter:
     def wsimport(self, *args):
         # getattr since import is special in python
         # NB RooWorkspace clones object
-        if len(args) < 2 :
+        if len(args) < 2:
             # Useless RooCmdArg: https://sft.its.cern.ch/jira/browse/ROOT-6785
-            args += (ROOT.RooCmdArg(),)
+            args += (ROOT.RooCmdArg(), )
         return getattr(self._w, 'import')(*args)
 
-    def set_fit_var(self, v='x', vMin=60, vMax=140, unit='GeV', label='m(#mu#mu)'):
+    def set_fit_var(self, v='x', vMin=60, vMax=140,
+                    unit='GeV', label='m(#mu#mu)'):
         self._fitVar = v
         self._fitVarMin = vMin
         self._fitVarMax = vMax
-        self._w.factory('{}[{}, {}]'.format(v,vMin,vMax))
-        if unit: self._w.var(v).setUnit(unit)
+        self._w.factory('{}[{}, {}]'.format(v, vMin, vMax))
+        if unit:
+            self._w.var(v).setUnit(unit)
         if label:
             self._w.var(v).setPlotLabel(label)
             self._w.var(v).SetTitle(label)
@@ -40,8 +43,12 @@ class TagAndProbeFitter:
         window = [int(pb-0.1*nb), int(pb+0.1*nb)]
         self._nPass_central = hPass.Integral(*window)
         self._nFail_central = hFail.Integral(*window)
-        dhPass = ROOT.RooDataHist('hPass', 'hPass', ROOT.RooArgList(self._w.var(self._fitVar)), hPass)
-        dhFail = ROOT.RooDataHist('hFail', 'hFail', ROOT.RooArgList(self._w.var(self._fitVar)), hFail)
+        dhPass = ROOT.RooDataHist(
+            'hPass', 'hPass',
+            ROOT.RooArgList(self._w.var(self._fitVar)), hPass)
+        dhFail = ROOT.RooDataHist(
+            'hFail', 'hFail',
+            ROOT.RooArgList(self._w.var(self._fitVar)), hFail)
         self.wsimport(dhPass)
         self.wsimport(dhFail)
 
@@ -53,8 +60,12 @@ class TagAndProbeFitter:
         window = [int(pb-0.1*nb), int(pb+0.1*nb)]
         self._nGenPass_central = hPass.Integral(*window)
         self._nGenFail_central = hFail.Integral(*window)
-        dhPass = ROOT.RooDataHist('hGenPass', 'hGenPass', ROOT.RooArgList(self._w.var(self._fitVar)), hPass)
-        dhFail = ROOT.RooDataHist('hGenFail', 'hGenFail', ROOT.RooArgList(self._w.var(self._fitVar)), hFail)
+        dhPass = ROOT.RooDataHist(
+            'hGenPass', 'hGenPass',
+            ROOT.RooArgList(self._w.var(self._fitVar)), hPass)
+        dhFail = ROOT.RooDataHist(
+            'hGenFail', 'hGenFail',
+            ROOT.RooArgList(self._w.var(self._fitVar)), hFail)
         self.wsimport(dhPass)
         self.wsimport(dhFail)
 
@@ -69,25 +80,32 @@ class TagAndProbeFitter:
         nPassHigh = 1.1*self._nPass
         nFailHigh = 1.1*self._nFail
 
-        # common lines, consider moving it outside in case we dont want to build the convolution when we fit PDF only
         if template:
-            self._w.factory("HistPdf::sigPhysPass({},hGenPass)".format(self._fitVar))
-            self._w.factory("HistPdf::sigPhysFail({},hGenFail)".format(self._fitVar))
-            self._w.factory("FCONV::sigPass({}, sigPhysPass , sigResPass)".format(self._fitVar))
-            self._w.factory("FCONV::sigFail({}, sigPhysFail , sigResFail)".format(self._fitVar))
+            self._w.factory(
+                "HistPdf::sigPhysPass({}, hGenPass)".format(self._fitVar))
+            self._w.factory(
+                "HistPdf::sigPhysFail({}, hGenFail)".format(self._fitVar))
+            self._w.factory(
+                "FCONV::sigPass({}, sigPhysPass , sigResPass)".format(
+                    self._fitVar))
+            self._w.factory(
+                "FCONV::sigFail({}, sigPhysFail , sigResFail)".format(
+                    self._fitVar))
             # update initial guesses
             nSigP = self._nGenPass_central / self._nGenPass * self._nPass
             nSigF = self._nGenFail_central / self._nGenFail * self._nFail
-            if nSigP<0.5: nSigP = 0.9*self._nPass
-            if nSigF<0.5: nSigF = 0.1*self._nFail
+            if nSigP < 0.5:
+                nSigP = 0.9 * self._nPass
+            if nSigF < 0.5:
+                nSigF = 0.1 * self._nFail
 
         # build extended pdf
-        self._w.factory("nSigP[{},0.5,{}]".format(nSigP,nPassHigh))
-        self._w.factory("nBkgP[{},0.5,{}]".format(nBkgP,nPassHigh))
-        self._w.factory("nSigF[{},0.5,{}]".format(nSigF,nFailHigh))
-        self._w.factory("nBkgF[{},0.5,{}]".format(nBkgF,nFailHigh))
-        self._w.factory("SUM::pdfPass(nSigP*sigPass,nBkgP*bkgPass)")
-        self._w.factory("SUM::pdfFail(nSigF*sigFail,nBkgF*bkgFail)")
+        self._w.factory("nSigP[{}, 0.5, {}]".format(nSigP, nPassHigh))
+        self._w.factory("nBkgP[{}, 0.5, {}]".format(nBkgP, nPassHigh))
+        self._w.factory("nSigF[{}, 0.5, {}]".format(nSigF, nFailHigh))
+        self._w.factory("nBkgF[{}, 0.5, {}]".format(nBkgF, nFailHigh))
+        self._w.factory("SUM::pdfPass(nSigP*sigPass, nBkgP*bkgPass)")
+        self._w.factory("SUM::pdfFail(nSigF*sigFail, nBkgF*bkgFail)")
 
     def fit(self, outFName, mcTruth=False, template=True):
 
@@ -103,11 +121,13 @@ class TagAndProbeFitter:
 
         # set the range on the fit var
         # needs to be smaller than the histogram range for the convolution
-        self._w.var(self._fitVar).setRange(self._fitRangeMin,self._fitRangeMax)
-        self._w.var(self._fitVar).setRange('fitRange',self._fitRangeMin,self._fitRangeMax)
+        self._w.var(self._fitVar).setRange(
+            self._fitRangeMin, self._fitRangeMax)
+        self._w.var(self._fitVar).setRange(
+            'fitRange', self._fitRangeMin, self._fitRangeMax)
 
         # fit passing histogram
-        resPass = pdfPass.fitTo(self._w.data("hPass"), 
+        resPass = pdfPass.fitTo(self._w.data("hPass"),
                                 ROOT.RooFit.Minos(self._useMinos),
                                 ROOT.RooFit.SumW2Error(True),
                                 ROOT.RooFit.Save(),
@@ -116,11 +136,14 @@ class TagAndProbeFitter:
 
         # when convolving, set fail sigma to fitted pass sigma
         if template:
-            self._w.var('sigmaF').setVal(self._w.var('sigmaP').getVal())
-            self._w.var('sigmaF').setRange(0.8*self._w.var('sigmaP').getVal(), 3.0*self._w.var('sigmaP').getVal())
+            self._w.var('sigmaF').setVal(
+                self._w.var('sigmaP').getVal())
+            self._w.var('sigmaF').setRange(
+                0.8 * self._w.var('sigmaP').getVal(),
+                3.0 * self._w.var('sigmaP').getVal())
 
         # fit failing histogram
-        resFail = pdfFail.fitTo(self._w.data("hFail"), 
+        resFail = pdfFail.fitTo(self._w.data("hFail"),
                                 ROOT.RooFit.Minos(self._useMinos),
                                 ROOT.RooFit.SumW2Error(True),
                                 ROOT.RooFit.Save(),
@@ -128,69 +151,73 @@ class TagAndProbeFitter:
                                 )
 
         # plot
-        pFrame = self._w.var(self._fitVar).frame(self._fitRangeMin,self._fitRangeMax)
+        pFrame = self._w.var(self._fitVar).frame(
+            self._fitRangeMin, self._fitRangeMax)
         pFrame.SetTitle('Passing probes')
         self._w.data('hPass').plotOn(pFrame)
         self._w.pdf('pdfPass').plotOn(pFrame,
                                       ROOT.RooFit.LineColor(ROOT.kRed),
                                       )
-        self._w.pdf('pdfPass').plotOn(pFrame, 
-                                      ROOT.RooFit.Components('bkgPass'), 
-                                      ROOT.RooFit.LineColor(ROOT.kBlue), 
+        self._w.pdf('pdfPass').plotOn(pFrame,
+                                      ROOT.RooFit.Components('bkgPass'),
+                                      ROOT.RooFit.LineColor(ROOT.kBlue),
                                       ROOT.RooFit.LineStyle(ROOT.kDashed),
                                       )
         self._w.data('hPass').plotOn(pFrame)
 
-        fFrame = self._w.var(self._fitVar).frame(self._fitRangeMin,self._fitRangeMax)
+        fFrame = self._w.var(self._fitVar).frame(
+            self._fitRangeMin, self._fitRangeMax)
         fFrame.SetTitle('Failing probes')
         self._w.data('hFail').plotOn(fFrame)
         self._w.pdf('pdfFail').plotOn(fFrame,
                                       ROOT.RooFit.LineColor(ROOT.kRed),
                                       )
-        self._w.pdf('pdfFail').plotOn(fFrame, 
-                                      ROOT.RooFit.Components('bkgFail'), 
-                                      ROOT.RooFit.LineColor(ROOT.kBlue), 
+        self._w.pdf('pdfFail').plotOn(fFrame,
+                                      ROOT.RooFit.Components('bkgFail'),
+                                      ROOT.RooFit.LineColor(ROOT.kBlue),
                                       ROOT.RooFit.LineStyle(ROOT.kDashed),
                                       )
         self._w.data('hFail').plotOn(fFrame)
 
         # make canvas
-        canvas = ROOT.TCanvas('c','c',1100,450)
-        canvas.Divide(3,1)
+        canvas = ROOT.TCanvas('c', 'c', 1100, 450)
+        canvas.Divide(3, 1)
 
         # print parameters
         canvas.cd(1)
         eff = -1
         e_eff = 0
-      
+
         nSigP = self._w.var("nSigP")
         nSigF = self._w.var("nSigF")
-      
-        nP   = nSigP.getVal()
+
+        nP = nSigP.getVal()
         e_nP = nSigP.getError()
-        nF   = nSigF.getVal()
+        nF = nSigF.getVal()
         e_nF = nSigF.getError()
-        nTot = nP+nF
-        eff = nP / (nP+nF)
-        e_eff = 1./(nTot*nTot) * ( nP*nP* e_nF*e_nF + nF*nF * e_nP*e_nP )**0.5
-      
-        text1 = ROOT.TPaveText(0,0.8,1,1)
+        nTot = nP + nF
+        eff = nP / (nP + nF)
+        e_eff = 1.0 / nTot * (e_nP**2 / nP**2 + e_nF**2 / nF**2)**0.5
+
+        text1 = ROOT.TPaveText(0, 0.8, 1, 1)
         text1.SetFillColor(0)
         text1.SetBorderSize(0)
         text1.SetTextAlign(12)
-      
-        text1.AddText("* fit status pass: {}, fail : {}".format(resPass.status(),resFail.status()))
-        text1.AddText("* eff = {:.4f} #pm {:.4f}".format(eff,e_eff))
-      
-        text = ROOT.TPaveText(0,0,1,0.8)
+
+        text1.AddText("* fit status pass: {}, fail : {}".format(
+            resPass.status(), resFail.status()))
+        text1.AddText("* eff = {:.4f} #pm {:.4f}".format(eff, e_eff))
+
+        text = ROOT.TPaveText(0, 0, 1, 0.8)
         text.SetFillColor(0)
         text.SetBorderSize(0)
         text.SetTextAlign(12)
-        text.AddText("    --- parameters " )
+        text.AddText("    --- parameters ")
 
         def argsetToList(argset):
             arglist = []
-            if not argset: return arglist
+            if not argset:
+                return arglist
             argiter = argset.createIterator()
             ax = argiter.Next()
             while ax:
@@ -202,14 +229,16 @@ class TagAndProbeFitter:
         for p in listParFinalP:
             pName = p.GetName()
             pVar = self._w.var(pName)
-            text.AddText("   - {} \t= {:.3f} #pm {:.3f}".format(pName, pVar.getVal(), pVar.getError()))
-      
+            text.AddText("   - {} \t= {:.3f} #pm {:.3f}".format(
+                pName, pVar.getVal(), pVar.getError()))
+
         listParFinalF = argsetToList(resFail.floatParsFinal())
         for p in listParFinalF:
             pName = p.GetName()
             pVar = self._w.var(pName)
-            text.AddText("   - {} \t= {:.3f} #pm {:.3f}".format(pName, pVar.getVal(), pVar.getError()))
-      
+            text.AddText("   - {} \t= {:.3f} #pm {:.3f}".format(
+                pName, pVar.getVal(), pVar.getError()))
+
         text1.Draw()
         text.Draw()
 
@@ -225,5 +254,4 @@ class TagAndProbeFitter:
         resPass.Write('{}_resP'.format(self._name), ROOT.TObject.kOverwrite)
         resFail.Write('{}_resF'.format(self._name), ROOT.TObject.kOverwrite)
         out.Close()
-        canvas.Print(outFName.replace('.root','.png'))
-
+        canvas.Print(outFName.replace('.root', '.png'))
--- run_single_fit.py
+++ run_single_fit.py
@@ -2,151 +2,162 @@
 from __future__ import print_function
 import os
 import sys
-import shutil
+from TagAndProbeFitter import TagAndProbeFitter
 import ROOT
 ROOT.gROOT.SetBatch()
-ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;") # kInfo = 1001, kWarning = 2001, ...
+# kInfo = 1001, kWarning = 2001, ...
+ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;")
 ROOT.gROOT.LoadMacro('RooCMSShape.cc+')
 
-from TagAndProbeFitter import TagAndProbeFitter
 
-def hist_fitter( outFName, inFName, binName, templateFName, plotDir, version='Nominal', histType='data', shiftType='Nominal'):
+def hist_fitter(outFName, inFName, binName, templateFName, plotDir,
+                version='Nominal', histType='data', shiftType='Nominal'):
 
     tnpNomFit = [
-        "meanP[-0.0,-5.0,5.0]","sigmaP[0.9,0.5,5.0]",
-        "meanF[-0.0,-5.0,5.0]","sigmaF[0.9,0.5,5.0]",
-        "acmsP[60.,50.,80.]","betaP[0.05,0.01,0.08]","gammaP[0.1, -2, 2]","peakP[90.0]",
-        "acmsF[60.,50.,80.]","betaF[0.05,0.01,0.08]","gammaF[0.1, -2, 2]","peakF[90.0]",
-        "Gaussian::sigResPass(x,meanP,sigmaP)",
-        "Gaussian::sigResFail(x,meanF,sigmaF)",
+        "meanP[-0.0, -5.0, 5.0]", "sigmaP[0.9, 0.5, 5.0]",
+        "meanF[-0.0, -5.0, 5.0]", "sigmaF[0.9, 0.5, 5.0]",
+        "acmsP[60., 50., 80.]", "betaP[0.05, 0.01, 0.08]",
+        "gammaP[0.1, -2, 2]", "peakP[90.0]",
+        "acmsF[60., 50., 80.]", "betaF[0.05, 0.01, 0.08]",
+        "gammaF[0.1, -2, 2]", "peakF[90.0]",
+        "Gaussian::sigResPass(x, meanP, sigmaP)",
+        "Gaussian::sigResFail(x, meanF, sigmaF)",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
         ]
 
     tnpNomFitOld = [
-        "meanP1[90.0,80.0,100.0]","sigmaP1[0.9,0.5,3.0]","widthP1[2.495]",
-        "meanF1[90.0,80.0,100.0]","sigmaF1[0.9,0.5,3.0]","widthF1[2.495]",
-        "meanP2[90.0.80.0.100.0]","sigmaP2[4.0,3.0,10.0]","widthP2[2.495]",
-        "meanF2[90.0.80.0.100.0]","sigmaF2[4.0,3.0,10.0]","widthF2[2.495]",
-        "acmsP[70.,50.,90.]","betaP[0.05,0.01,0.08]","gammaP[0.1, -2, 2]","peakP[90.0]",
-        "acmsF[70.,50.,90.]","betaF[0.05,0.01,0.08]","gammaF[0.1, -2, 2]","peakF[90.0]",
+        "meanP1[90.0, 80.0, 100.0]", "sigmaP1[0.9, 0.5, 3.0]",
+        "widthP1[2.495]",
+        "meanF1[90.0, 80.0, 100.0]", "sigmaF1[0.9, 0.5, 3.0]",
+        "widthF1[2.495]",
+        "meanP2[90.0.80.0.100.0]", "sigmaP2[4.0, 3.0, 10.0]",
+        "widthP2[2.495]",
+        "meanF2[90.0.80.0.100.0]", "sigmaF2[4.0, 3.0, 10.0]",
+        "widthF2[2.495]",
+        "acmsP[70., 50., 90.]", "betaP[0.05, 0.01, 0.08]",
+        "gammaP[0.1, -2, 2]", "peakP[90.0]",
+        "acmsF[70., 50., 90.]", "betaF[0.05, 0.01, 0.08]",
+        "gammaF[0.1, -2, 2]", "peakF[90.0]",
         "Voigtian::sigPass1(x, meanP1, widthP1, sigmaP1)",
         "Voigtian::sigFail1(x, meanF1, widthF1, sigmaF1)",
         "Voigtian::sigPass2(x, meanP2, widthP2, sigmaP2)",
         "Voigtian::sigFail2(x, meanF2, widthF2, sigmaF2)",
-        "SUM::sigPass(fP[0.7,0.5,1]*sigPass1, sigPass2)",
-        "SUM::sigFail(fF[0.7,0.5,1]*sigFail1, sigFail2)",
+        "SUM::sigPass(fP[0.7, 0.5, 1]*sigPass1, sigPass2)",
+        "SUM::sigFail(fF[0.7, 0.5, 1]*sigFail1, sigFail2)",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
         ]
 
     tnpAltSigFit = [
-        "meanP[-0.0,-5.0,5.0]","sigmaP[1,0.7,6.0]","alphaP[2.0,1.2,3.5]" ,'nP[3,-5,5]',"sigmaP_2[1.5,0.5,6.0]","sosP[1,0.5,5.0]",
-        "meanF[-0.0,-5.0,5.0]","sigmaF[2,0.7,15.0]","alphaF[2.0,1.2,3.5]",'nF[3,-5,5]',"sigmaF_2[2.0,0.5,6.0]","sosF[1,0.5,5.0]",
-        "acmsP[60.,50.,75.]","betaP[0.04,0.01,0.06]","gammaP[0.1, 0.005, 1]","peakP[90.0]",
-        "acmsF[60.,50.,75.]","betaF[0.04,0.01,0.06]","gammaF[0.1, 0.005, 1]","peakF[90.0]",
-        # EGM
-        #"tailLeft[1]",
-        #"RooCBExGaussShape::sigResPass(x,meanP,expr('sqrt(sigmaP*sigmaP+sosP*sosP)',{sigmaP,sosP}),alphaP,nP, expr('sqrt(sigmaP_2*sigmaP_2+sosP*sosP)',{sigmaP_2,sosP}),tailLeft)",
-        #"RooCBExGaussShape::sigResFail(x,meanF,expr('sqrt(sigmaF*sigmaF+sosF*sosF)',{sigmaF,sosF}),alphaF,nF, expr('sqrt(sigmaF_2*sigmaF_2+sosF*sosF)',{sigmaF_2,sosF}),tailLeft)",
+        "meanP[-0.0, -5.0, 5.0]", "sigmaP[1, 0.7, 6.0]",
+        "alphaP[2.0, 1.2, 3.5]",
+        'nP[3, -5, 5]', "sigmaP_2[1.5, 0.5, 6.0]", "sosP[1, 0.5, 5.0]",
+        "meanF[-0.0, -5.0, 5.0]", "sigmaF[2, 0.7, 15.0]",
+        "alphaF[2.0, 1.2, 3.5]",
+        'nF[3, -5, 5]', "sigmaF_2[2.0, 0.5, 6.0]", "sosF[1, 0.5, 5.0]",
+        "acmsP[60., 50., 75.]", "betaP[0.04, 0.01, 0.06]",
+        "gammaP[0.1, 0.005, 1]", "peakP[90.0]",
+        "acmsF[60., 50., 75.]", "betaF[0.04, 0.01, 0.06]",
+        "gammaF[0.1, 0.005, 1]", "peakF[90.0]",
         # alternative for faster fitting, just use gen truth for template
-        "Gaussian::sigResPass(x,meanP,sigmaP)",
-        "Gaussian::sigResFail(x,meanF,sigmaF)",
+        "Gaussian::sigResPass(x, meanP, sigmaP)",
+        "Gaussian::sigResFail(x, meanF, sigmaF)",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
         ]
 
     tnpAltSigFitOld = [
-        "meanP[90.0,80.0,100.0]","sigmaP[0.9,0.5,5.0]","widthP[2.495]",
-        "meanF[90.0,80.0,100.0]","sigmaF[0.9,0.5,5.0]","widthF[2.495]",
-        "acmsP[60.,50.,80.]","betaP[0.05,0.01,0.08]","gammaP[0.1, -2, 2]","peakP[90.0]",
-        "acmsF[60.,50.,80.]","betaF[0.05,0.01,0.08]","gammaF[0.1, -2, 2]","peakF[90.0]",
+        "meanP[90.0, 80.0, 100.0]", "sigmaP[0.9, 0.5, 5.0]", "widthP[2.495]",
+        "meanF[90.0, 80.0, 100.0]", "sigmaF[0.9, 0.5, 5.0]", "widthF[2.495]",
+        "acmsP[60., 50., 80.]", "betaP[0.05, 0.01, 0.08]",
+        "gammaP[0.1, -2, 2]", "peakP[90.0]",
+        "acmsF[60., 50., 80.]", "betaF[0.05, 0.01, 0.08]",
+        "gammaF[0.1, -2, 2]", "peakF[90.0]",
         "Voigtian::sigPass(x, meanP, widthP, sigmaP)",
         "Voigtian::sigFail(x, meanF, widthF, sigmaF)",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
         ]
-    
+
     tnpAltBkgFit = [
-        "meanP[-0.0,-5.0,5.0]","sigmaP[0.9,0.5,5.0]",
-        "meanF[-0.0,-5.0,5.0]","sigmaF[0.9,0.5,5.0]",
-        "alphaP[0.,-5.,5.]",
-        "alphaF[0.,-5.,5.]",
-        "Gaussian::sigResPass(x,meanP,sigmaP)",
-        "Gaussian::sigResFail(x,meanF,sigmaF)",
+        "meanP[-0.0, -5.0, 5.0]", "sigmaP[0.9, 0.5, 5.0]",
+        "meanF[-0.0, -5.0, 5.0]", "sigmaF[0.9, 0.5, 5.0]",
+        "alphaP[0., -5., 5.]",
+        "alphaF[0., -5., 5.]",
+        "Gaussian::sigResPass(x, meanP, sigmaP)",
+        "Gaussian::sigResFail(x, meanF, sigmaF)",
         "Exponential::bkgPass(x, alphaP)",
         "Exponential::bkgFail(x, alphaF)",
         ]
 
-
     tnpWorkspace = []
     doTemplate = True
-    if version=='Nominal':
+    if version == 'Nominal':
         tnpWorkspace.extend(tnpNomFit)
-    if version=='NominalOld':
+    if version == 'NominalOld':
         tnpWorkspace.extend(tnpNomFitOld)
         doTemplate = False
-    if version=='AltSigOld':
+    if version == 'AltSigOld':
         tnpWorkspace.extend(tnpAltSigFitOld)
         doTemplate = False
-    elif version=='AltSig':
+    elif version == 'AltSig':
         tnpWorkspace.extend(tnpAltSigFit)
-    elif version=='AltBkg':
+    elif version == 'AltBkg':
         tnpWorkspace.extend(tnpAltBkgFit)
 
-
-    def rebin(hP,hF):
-        if shiftType=='massBinUp':
-            pass # no rebin, bin widths are 0.5 GeV
-        elif shiftType=='massBinDown':
-            hP = hP.Rebin(3) # 1.5 GeV bins
-            hF = hF.Rebin(3) # 1.5 GeV bins
+    def rebin(hP, hF):
+        if shiftType == 'massBinUp':
+            pass  # no rebin, bin widths are 0.5 GeV
+        elif shiftType == 'massBinDown':
+            hP = hP.Rebin(3)  # 1.5 GeV bins
+            hF = hF.Rebin(3)  # 1.5 GeV bins
         else:
-            hP = hP.Rebin(2) # 1.0 GeV bins
-            hF = hF.Rebin(2) # 1.0 GeV bins
+            hP = hP.Rebin(2)  # 1.0 GeV bins
+            hF = hF.Rebin(2)  # 1.0 GeV bins
         return hP, hF
 
-    ## init fitter
-    infile = ROOT.TFile( inFName, "read")
-    hP = infile.Get('%s_Pass' % binName )
-    hF = infile.Get('%s_Fail' % binName )
+    # init fitter
+    infile = ROOT.TFile(inFName, "read")
+    hP = infile.Get(f'{binName}_Pass')
+    hF = infile.Get(f'{binName}_Fail')
     hP, hF = rebin(hP, hF)
     fitter = TagAndProbeFitter(binName)
-    fitter.set_histograms(hP,hF)
+    fitter.set_histograms(hP, hF)
     infile.Close()
 
     # mass range systematic
-    if shiftType=='massRangeUp':
-        fitter.set_fit_range(75,135)
-    elif shiftType=='massRangeDown':
-        fitter.set_fit_range(65,125)
+    if shiftType == 'massRangeUp':
+        fitter.set_fit_range(75, 135)
+    elif shiftType == 'massRangeDown':
+        fitter.set_fit_range(65, 125)
     else:
-        fitter.set_fit_range(70,130)
+        fitter.set_fit_range(70, 130)
 
-    ## setup
+    # setup
     try:
         os.makedirs(os.path.dirname(outFName))
     except OSError:
         pass
 
-    ## generated Z LineShape
-    ## for high pT change the failing spectra to any probe to get statistics
-    fileTruth  = ROOT.TFile(templateFName,'read')
-    if version=='AltSig':
+    # generated Z LineShape
+    # for high pT change the failing spectra to any probe to get statistics
+    fileTruth = ROOT.TFile(templateFName, 'read')
+    if version == 'AltSig':
         # TODO: truth file for ZmmGenLevel instead of reco in case of AltSig
-        histZLineShapeP = fileTruth.Get('%s_Pass_Gen'%binName)
-        histZLineShapeF = fileTruth.Get('%s_Fail_Gen'%binName)
+        histZLineShapeP = fileTruth.Get(f'{binName}_Pass_Gen')
+        histZLineShapeF = fileTruth.Get(f'{binName}_Fail_Gen')
     else:
-        histZLineShapeP = fileTruth.Get('%s_Pass'%binName)
-        histZLineShapeF = fileTruth.Get('%s_Fail'%binName)
-    histZLineShapeP, histZLineShapeF = rebin(histZLineShapeP,histZLineShapeF)
-    fitter.set_gen_shapes(histZLineShapeP,histZLineShapeF)
+        histZLineShapeP = fileTruth.Get(f'{binName}_Pass')
+        histZLineShapeF = fileTruth.Get(f'{binName}_Fail')
+    histZLineShapeP, histZLineShapeF = rebin(histZLineShapeP, histZLineShapeF)
+    fitter.set_gen_shapes(histZLineShapeP, histZLineShapeF)
 
     fileTruth.Close()
 
-    ### set workspace
+    # set workspace
     fitter.set_workspace(tnpWorkspace, doTemplate)
-    fitter.fit(outFName, histType=='mc', doTemplate)
+    fitter.fit(outFName, histType == 'mc', doTemplate)
 
 
 # TODO other fits and argparse
