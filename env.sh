68767c03eb10384f5c6a7accf702b6b32969f9f7
Devin Taylor <dntaylor@ucdavis.edu>
2020-05-27 00:50:08 +0200

    bump LCG release for uproot

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-LCG_RELEASE=LCG_96bpython3
+LCG_RELEASE=LCG_97python3
 LCG_ARCH=x86_64-centos7-gcc8
 source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${LCG_ARCH}-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh analytix
