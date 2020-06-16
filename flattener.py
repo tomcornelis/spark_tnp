3d23561a6f00bd17fa58fe0d2531f0feab6797ea
Jess Wong Wing Yan <wing.yan.wong@cern.ch>
2020-06-16 10:38:23 +0200

    Modified for miniIso after pulling updates

--- flattener.py
+++ flattener.py
@@ -10,6 +10,7 @@ from uproot_methods.classes import TH1
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql import types as types
 
 from registry import registry
 from muon_definitions import (get_weighted_dataframe,
@@ -70,6 +71,15 @@ def run_conversion(spark, particle, probe, resonance, era, subEra,
     # create the definitions columns
     definitions = config.definitions()
     defDF = baseDF
+    #### add columns with miniIsolation information
+    MiniIsoAEff_udf = F.udf(lambda abseta : 0.0735 if abseta <= 0.8 else (0.0619 if abseta <= 1.3 else (0.0465 if  abseta <= 2.0 else (0.0433 if abseta <=2.2 else 0.0577) ) ) , types.FloatType())
+    MiniIsoRiso2_udf = F.udf(lambda pt : max(0.05, min(0.2, 10.0/pt)), types.FloatType())
+    MiniIsolation_udf = F.udf(lambda charged, photon, neutral, corr, pt : (charged+max(0.0,photon+neutral-corr))/pt , types.FloatType())
+    defDF = defDF.withColumn('MiniIsoAEff', MiniIsoAEff_udf(defDF.abseta) )
+    defDF = defDF.withColumn('MiniIso_riso2', MiniIsoRiso2_udf(defDF.pt) )
+    defDF = defDF.withColumn('MiniIso_CorrectedTerm', F.col('fixedGridRhoFastjetCentralNeutral')*F.col('MiniIsoAEff')*(F.col('MiniIso_riso2')/0.09) )
+    defDF = defDF.withColumn('MiniIsolation', MiniIsolation_udf(defDF.miniIsoCharged, defDF.miniIsoPhotons, defDF.miniIsoNeutrals, defDF.MiniIso_CorrectedTerm, defDF.pt) ) 
+    ####
     for d in definitions:
         defDF = defDF.withColumn(d, F.expr(definitions[d]))
 
