f2849cba458dd6b5123a1e5296274d2108b3e53d
Devin Taylor <dntaylor@ucdavis.edu>
2020-06-22 05:06:26 +0200

    modify single fit for consistency in initial params

--- run_single_fit.py
+++ run_single_fit.py
@@ -13,20 +13,24 @@ ROOT.gROOT.LoadMacro('RooCMSShape.cc+')
 def hist_fitter(outFName, inFName, binName, templateFName, plotDir,
                 version='Nominal', histType='data', shiftType='Nominal'):
 
-    tnpNomFit = [
+    # Nominal
+    tnpNomFitSig = [
         "meanP[-0.0, -5.0, 5.0]", "sigmaP[0.9, 0.05, 5.0]",
         "meanF[-0.0, -5.0, 5.0]", "sigmaF[0.9, 0.05, 5.0]",
+        "Gaussian::sigResPass(x, meanP, sigmaP)",
+        "Gaussian::sigResFail(x, meanF, sigmaF)",
+    ]
+    tnpNomFitBkg = [
         "acmsP[60., 50., 190.]", "betaP[0.05, 0.01, 0.08]",
         "gammaP[0.1, -2, 2]", "peakP[91.0]",
         "acmsF[60., 50., 190.]", "betaF[0.05, 0.01, 0.08]",
         "gammaF[0.1, -2, 2]", "peakF[91.0]",
-        "Gaussian::sigResPass(x, meanP, sigmaP)",
-        "Gaussian::sigResFail(x, meanF, sigmaF)",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
-        ]
+    ]
 
-    tnpNomFitOld = [
+    # NominalOld
+    tnpNomFitOldSig = [
         "meanP1[90.0, 80.0, 100.0]", "sigmaP1[0.9, 0.5, 3.0]",
         "widthP1[2.495]",
         "meanF1[90.0, 80.0, 100.0]", "sigmaF1[0.9, 0.5, 3.0]",
@@ -35,20 +39,23 @@ def hist_fitter(outFName, inFName, binName, templateFName, plotDir,
         "widthP2[2.495]",
         "meanF2[90.0.80.0.100.0]", "sigmaF2[4.0, 3.0, 10.0]",
         "widthF2[2.495]",
-        "acmsP[70., 50., 90.]", "betaP[0.05, 0.01, 0.08]",
-        "gammaP[0.1, -2, 2]", "peakP[90.0]",
-        "acmsF[70., 50., 90.]", "betaF[0.05, 0.01, 0.08]",
-        "gammaF[0.1, -2, 2]", "peakF[90.0]",
         "Voigtian::sigPass1(x, meanP1, widthP1, sigmaP1)",
         "Voigtian::sigFail1(x, meanF1, widthF1, sigmaF1)",
         "Voigtian::sigPass2(x, meanP2, widthP2, sigmaP2)",
         "Voigtian::sigFail2(x, meanF2, widthF2, sigmaF2)",
         "SUM::sigPass(fP[0.7, 0.5, 1]*sigPass1, sigPass2)",
         "SUM::sigFail(fF[0.7, 0.5, 1]*sigFail1, sigFail2)",
+    ]
+    tnpNomFitOldBkg = [
+        "acmsP[70., 50., 90.]", "betaP[0.05, 0.01, 0.08]",
+        "gammaP[0.1, -2, 2]", "peakP[90.0]",
+        "acmsF[70., 50., 90.]", "betaF[0.05, 0.01, 0.08]",
+        "gammaF[0.1, -2, 2]", "peakF[90.0]",
         "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
         "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
-        ]
+    ]
 
+    # AltSig (note, originally was CB for res, but took too long)
     tnpAltSigFit = [
         "meanP[-0.0, -5.0, 5.0]", "sigmaP[1, 0.7, 6.0]",
         "alphaP[2.0, 1.2, 3.5]",
@@ -56,54 +63,45 @@ def hist_fitter(outFName, inFName, binName, templateFName, plotDir,
         "meanF[-0.0, -5.0, 5.0]", "sigmaF[2, 0.7, 15.0]",
         "alphaF[2.0, 1.2, 3.5]",
         'nF[3, -5, 5]', "sigmaF_2[2.0, 0.5, 6.0]", "sosF[1, 0.5, 5.0]",
-        "acmsP[60., 50., 75.]", "betaP[0.04, 0.01, 0.06]",
-        "gammaP[0.1, 0.005, 1]", "peakP[90.0]",
-        "acmsF[60., 50., 75.]", "betaF[0.04, 0.01, 0.06]",
-        "gammaF[0.1, 0.005, 1]", "peakF[90.0]",
         # alternative for faster fitting, just use gen truth for template
         "Gaussian::sigResPass(x, meanP, sigmaP)",
         "Gaussian::sigResFail(x, meanF, sigmaF)",
-        "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
-        "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
-        ]
+    ]
 
+    # AltSigOld
     tnpAltSigFitOld = [
         "meanP[90.0, 80.0, 100.0]", "sigmaP[0.9, 0.5, 5.0]", "widthP[2.495]",
         "meanF[90.0, 80.0, 100.0]", "sigmaF[0.9, 0.5, 5.0]", "widthF[2.495]",
-        "acmsP[60., 50., 80.]", "betaP[0.05, 0.01, 0.08]",
-        "gammaP[0.1, -2, 2]", "peakP[90.0]",
-        "acmsF[60., 50., 80.]", "betaF[0.05, 0.01, 0.08]",
-        "gammaF[0.1, -2, 2]", "peakF[90.0]",
         "Voigtian::sigPass(x, meanP, widthP, sigmaP)",
         "Voigtian::sigFail(x, meanF, widthF, sigmaF)",
-        "RooCMSShape::bkgPass(x, acmsP, betaP, gammaP, peakP)",
-        "RooCMSShape::bkgFail(x, acmsF, betaF, gammaF, peakF)",
-        ]
+    ]
 
+    # AltBkg
     tnpAltBkgFit = [
-        "meanP[-0.0, -5.0, 5.0]", "sigmaP[0.9, 0.5, 5.0]",
-        "meanF[-0.0, -5.0, 5.0]", "sigmaF[0.9, 0.5, 5.0]",
         "alphaP[0., -5., 5.]",
         "alphaF[0., -5., 5.]",
-        "Gaussian::sigResPass(x, meanP, sigmaP)",
-        "Gaussian::sigResFail(x, meanF, sigmaF)",
         "Exponential::bkgPass(x, alphaP)",
         "Exponential::bkgFail(x, alphaF)",
-        ]
+    ]
 
     tnpWorkspace = []
     doTemplate = True
     if version == 'Nominal':
-        tnpWorkspace.extend(tnpNomFit)
+        tnpWorkspace.extend(tnpNomFitSig)
+        tnpWorkspace.extend(tnpNomFitBkg)
     if version == 'NominalOld':
-        tnpWorkspace.extend(tnpNomFitOld)
+        tnpWorkspace.extend(tnpNomFitOldSig)
+        tnpWorkspace.extend(tnpNomFitOldBkg)
         doTemplate = False
     if version == 'AltSigOld':
         tnpWorkspace.extend(tnpAltSigFitOld)
+        tnpWorkspace.extend(tnpNomFitOldBkg)
         doTemplate = False
     elif version == 'AltSig':
         tnpWorkspace.extend(tnpAltSigFit)
+        tnpWorkspace.extend(tnpNomFitBkg)
     elif version == 'AltBkg':
+        tnpWorkspace.extend(tnpNomFitSig)
         tnpWorkspace.extend(tnpAltBkgFit)
 
     def rebin(hP, hF):
